beedddb10d5b14dc794a22f043e02a26de7592c9
Erica <ericabasak@gmail.com>
2019-09-06 20:23:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,14 +1,15 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Laundry", "Dishes", "Pack for trip", "Buy rain jacket"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push("arrays!")
 end
+
 
 def add_element_to_start_of_array(array, element)
   
@@ -23,7 +24,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  puts array[]
 end
 
 def retrieve_first_element_from_array(array)
